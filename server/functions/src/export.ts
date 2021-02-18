c82a5f6a0d8a7c2585d2a6ab0c04564f89e70c46
rlnsy <rowlindsay@gmail.com>
2021-02-18 00:27:23 -0800

    implement remote services calls

--- server/functions/src/export.ts
+++ server/functions/src/export.ts
@@ -1,14 +1,18 @@
 import { FeatureSettings } from './services/config';
 import * as functions from 'firebase-functions';
+import { logNewAccount } from './utils/remoteServices';
 
 const fns: any = {};
 
 fns.newAccount = FeatureSettings.ExportToDataServices
     && functions.firestore.document('/clients/{clientId}/accounts/{acctId}')
         // Database-event based export
-      .onCreate((snap, context) => {
+      .onCreate(async(snap, context) => {
         const acct = snap.data();
-        console.log(`New account for client[${context.params.clientId}], ${JSON.stringify(acct)}`);
+        const client = context.params.clientId;
+        const coach = acct['coachId'];
+        console.log(`New account for client[${client}], coach[${coach}]`);
+        await logNewAccount(client, coach);
         return null;
       });
 
---
+++ server/functions/src/utils/remoteServices.ts
@@ -0,0 +1,24 @@
+import Axios from 'axios';
+
+const ax = Axios.create({
+	baseURL: "http://127.0.0.1:8888" // TODO
+});
+
+const API_KEY = "apikey1"
+
+export async function logNewAccount(clientID, coachID) {
+    await ax.post("/account",
+        { clientID, coachID }, 
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": API_KEY
+            }
+        })
+        .then((r) => {
+            console.log(`Done!`)
+        })
+        .catch((e) => {
+            console.log(`Error reaching service: ${e}`);
+        });
+}
