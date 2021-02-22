5efb46c992af0672de422c0110593ab9f65b58ae
rlnsy <rowlindsay@gmail.com>
2021-02-22 14:51:45 -0800

    refactor export code

--- server/functions/src/export.ts
+++ server/functions/src/export.ts
@@ -17,28 +17,50 @@ fns.newAccount = FeatureSettings.ExportToDataServices
         return null;
       });
 
+type RecordExport = {
+    typeId: string,
+    value: number
+};
+
+type RecordMapping = (d: any) => RecordExport[];
+
+type Extractions = {
+    [key: string] : RecordMapping
+};
+
+const prefix = (id: string): string => `maslo-${id}`;
+
+const extract: Extractions = {
+    // mappings for complex data types
+    'mindfulness': 
+        (d: any): RecordExport[] => [
+            { typeId: prefix('mindfulness'), value: +d }
+        ],
+    'mentalHealth': 
+        (d: any): RecordExport[] => [
+            { typeId: prefix('mentalHealth'), value: +d }
+        ],
+}
+
 fns.measurement = FeatureSettings.ExportToDataServices
 && functions.firestore.document('/records/{recordId}')
       .onCreate(async(snap, context) => {
           const data = snap.data();
-          const quantFields = [
-            'mentalHealth',
-            'mindfulness',
-          ];
+          const makeRequest = (ex: RecordExport) => logMeasurement(
+                data.clientUid, data.coachUid, ex.typeId, ex.value, data.date);
+                
           // Q: should we mash into a single call?
-          await Promise.all(quantFields.map(
-              key => {
-                  const typeId = `maslo-fb-${key}`; // TODO: figure out universal ids
-                  const val = data[key];
-                  if (val) {
-                    return logMeasurement(data.clientUid, data.coachUid, typeId, val, data.date);
-                  } else if (val == null) {
-                    // value not recorded. Simply skip in this case
-                    return Promise.resolve();
-                  } else {
-                    return Promise.reject(`Key ${key} is not valid for record`);
-                  }
-              }));
+          await Promise.all(Object.entries(extract).reduce((ps, [key, ext]) => {
+            const val = data[key];
+            if (val) {
+                return ps.concat(ext(val).map(e => makeRequest(e)));
+              } else if (val == null) {
+                // value not recorded. Simply skip in this case
+                return ps.concat([Promise.resolve()]);
+              } else {
+                return ps.concat([Promise.reject(`Key ${key} is not valid for record`)]);
+              }
+          }, ([] as Promise<void>[])));
       });
 
 export const ExportFunctions = FeatureSettings.ExportToDataServices && fns;
