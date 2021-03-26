0b4857082f79e47a78e4954c12e08b38e2d8ad8a
rlnsy <rowlindsay@gmail.com>
2021-03-26 13:15:44 -0700

    update client mocks

---
+++ server/functions/__tests__/mocks/client/process.ts
@@ -0,0 +1,11 @@
+process.env.APP_ENV = 'staging';
+process['firebaseForPlatform'] = {
+    android: {
+        apiKey: 'AIzaSyD0Pj0seTSU-n8OMPuZwxkcs0WoBm5boPU'
+    },
+    ios: {
+
+    }
+};
+
+export default process;
