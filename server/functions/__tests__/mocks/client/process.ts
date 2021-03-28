b8849ddca17b07a9a42615a6b0d9f1af0bef01ad
rlnsy <rowlindsay@gmail.com>
2021-03-28 16:42:14 -0700

    remove hard-coded api key

--- server/functions/__tests__/mocks/client/process.ts
+++ server/functions/__tests__/mocks/client/process.ts
@@ -1,11 +1,7 @@
 process.env.APP_ENV = 'staging';
 process['firebaseForPlatform'] = {
-    android: {
-        apiKey: 'AIzaSyD0Pj0seTSU-n8OMPuZwxkcs0WoBm5boPU'
-    },
-    ios: {
-
-    }
+    android: {},
+    ios: {}
 };
 
 export default process;
