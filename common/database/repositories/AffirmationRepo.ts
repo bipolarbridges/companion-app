72b188fdaa7b258b1574ce3fb7ed6ab82e732dd0
kaede <kaede.ito2001@gmail.com>
2021-07-02 17:09:41 -0700

    fix lint

--- common/database/repositories/AffirmationRepo.ts
+++ common/database/repositories/AffirmationRepo.ts
@@ -10,21 +10,17 @@ export default class AffirmationRepo extends GenericRepo<Affirmation> {
         return Collections.Affirmations;
     }
 
-    async getByDomain(domains: string[], keywordFilter?: string[]):Promise<Maybe<Affirmation[]>> {
+    async getByDomain(domains: string[], keywordFilter: string[]): Promise<Maybe<Affirmation[]>> {
         const query: Query = this.collection
             .where(nameof<Affirmation>(a => a.domains), 'array-contains-any', domains);
         const docs: DocumentSnapshot[] = (await query.get()).docs;
         if (docs.length < 1) {
             return null;
         } else {
-            const data = docs.map((af) => {return af.data() as Affirmation });
-            let result = data;
-            if (keywordFilter && keywordFilter.length > 0) {
-                result = data.filter((af) => {
-                    !af.keywords.some(r=> keywordFilter.includes(r))
-                });
-            }
-            return result;
+            const data = docs.map((af) => {return af.data() as Affirmation; });
+            return data.filter((af) => {
+                return !af.keywords.some(r => keywordFilter.includes(r));
+            });
         }
     }
 }
