1fdbabdcf256e8efd61572aad9f09bc688db3110
Mapleia <mapleia01@gmail.com>
2021-07-05 14:36:08 -0700

    fixed eslint config

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-    root: true,
-    extends: '@react-native-community',
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    ignorePatterns: ['./dependencies/'],
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['dependencies/*'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 0,
+    'react-native/no-inline-styles': 0,
+  }
 };
